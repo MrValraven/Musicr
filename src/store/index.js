30e8f1e3c1060ace386faa11d925ffdb41ddf88b
Tiago Costa <akatiago@gmail.com>
2021-08-08 15:46:49 +0100

    init

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
+
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    }
   },
+
+  getters: {
+    // authModalShow: (state) =>  state.authModalShow,
+  },
+
+
+
+
   actions: {
   },
   modules: {
